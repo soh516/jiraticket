b9fa7ba3e363ba6ab6a8257d4161dc7d45d890d4
song.hu@usask.ca <song.hu@usask.ca>
2022-07-10 19:14:45 -0600

    working version

--- jiraSearchString.py
+++ jiraSearchString.py
@@ -8,6 +8,21 @@ start_year = lastMonth.strftime('%Y')
 start_month = lastMonth.strftime('%m')
 start_day = input('Enter the start day:')
 
+while True:
+    try:
+        int(start_day)
+    except ValueError: 
+        print('Must be interger!!')
+        exit(1)
+    else:
+        if (1 <= int(start_day) <= 31):
+            break
+        else:
+            print('Out of range!!')
+            exit(1)
+
+print("")
+
 current_year = datetime.now().strftime('%Y')
 current_month = datetime.now().strftime('%m')
 current_day = datetime.now().strftime('%d')
@@ -15,7 +30,7 @@ current_day = datetime.now().strftime('%d')
 start_date = start_year + '-' + start_month + '-' + start_day
 end_date = current_year + '-' + current_month + '-' + current_day
 
-core_nsid = ["mod697", "jdc210"]
+general_nsid = ["mod697", "jdc210"]
 hpc_nsid = ["olf067", "ses863", "bmb760", "bob521"]
 
 
@@ -62,18 +77,31 @@ print("linux search string:\n" + linux.getLinuxSearchString() + "\n")
 redcap = getString("", start_date, end_date)
 print("redcap search string:\n" + redcap.getRedcapSearchString() + "\n")
 
-if len(core_nsid) == 1:
-    general = getString(core_nsid[0], start_date, end_date)
-    print(general.getGeneralSearchString() + "\n")
+if len(general_nsid) == 1:
+    general = getString(general_nsid[0], start_date, end_date)
+    general_string = general.getGeneralSearchString()
 else:
-    for x in core_nsid:
-        general = getString(x, start_date, end_date)
-        print(general.getGeneralSearchString() + "\n")
+    for i in range(len(general_nsid)):
+        if i == 0:
+            general = getString(general_nsid[i], start_date, end_date)
+            general_string = "(" + general.getGeneralSearchString() + ")"
+        else:
+            general = getString(general_nsid[i], start_date, end_date)
+            general_string = general_string + " OR " + "(" + general.getGeneralSearchString() + ")"
+
+print("general search string:\n" + general_string + "\n")
+
 
 if len(hpc_nsid) == 1:
     hpc = getString(hpc_nsid[0], start_date, end_date)
     print(hpc.getHPCSearchString() + "\n")
 else:
-    for x in hpc_nsid:
-        hpc = getString(x, start_date, end_date)
-        print(hpc.getHPCSearchString() + "\n")
+    for i in range(len(hpc_nsid)):
+        if i == 0:
+            hpc = getString(hpc_nsid[i], start_date, end_date)
+            hpc_string = "(" + hpc.getHPCSearchString() + ")"
+        else:
+            hpc = getString(hpc_nsid[i], start_date, end_date)
+            hpc_string = hpc_string + " OR " + "(" + hpc.getHPCSearchString() + ")"
+
+print("hpc string:\n" + hpc_string + "\n")
